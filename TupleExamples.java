bab5a5b44b700ddc4146f1a772bc5bc0b6cbc658
Teja Swaroop Arukoti <tejaswarooparukoti@gmail.com>
2021-09-04 20:32:38 +0530

    added tuple

---
+++ TupleExamples.java
@@ -0,0 +1,9 @@
+
+import org.javatuples.*;
+public class TupleExamples {
+    public static void test () {
+        System.out.println("Tuples Test");
+        Pair<Integer,String> myPair = new Pair<Integer,String>(Integer.valueOf(1),"Teja");
+        System.out.println(myPair);
+    }
+}
